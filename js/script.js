073b3d1ecf8f5adb04d79c8d88ebb8de04ed0e77
Anna <example@gmail.com>
2020-06-24 14:01:18 +0500

    add 2 section

--- js/script.js
+++ js/script.js
@@ -15,12 +15,50 @@
 //       ]
 //   });
 // });
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        items: 1,
-        nav: true,
-        navText: ["<img src='img/icons/arrow_left.png'>", "<img src='img/Icons/arrow_right.png'>"],
-        dots: false
-    }
-    );
+// $(document).ready(function(){
+//     $(".owl-carousel").owlCarousel({
+//         items: 1,
+//         autoHeight:true,
+//         loop: true,
+//         nav: true,
+//         navText: ["<img src='img/icons/arrow_left.png'>", "<img src='img/Icons/arrow_right.png'>"],
+//         dots: false,
+//         responsive:{
+//         900:{
+//             nav: false
+//         }
+//     }  
+//     }
+//     )
+//   });
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    loop: true,
+    controls: false,
+    nav: false,
+    responsive: {
+        640: {
+            nav: true
+        },
+        700: {
+            // edgePadding: 30,
+            nav: true
+        },
+        900: {
+          nav: true,
+          navPosition: "bottom",
+          edgePadding: 30
+        }
+      }
   });
+document.querySelector('.prev').addEventListener ('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener ('click', function () {
+    slider.goTo('next');
+});
